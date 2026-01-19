d56e97411060fe4de1b4b0dbb5028792e2cb65a5
Marco Osorio <Marco.osorio86@hotmail.com>
2026-01-19 15:27:13 -0500

    Se agregan excepciones a los métodos de la interfaz

--- app/src/main/java/jdbc/introduction/repository/Repository.java
+++ app/src/main/java/jdbc/introduction/repository/Repository.java
@@ -1,17 +1,14 @@
 package jdbc.introduction.repository;
 
-import jdbc.introduction.model.Employee;
-
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface Repository<T> {
-    List<T> getAll() throws SQLException;
+    List<T> getAll() throws Exception;
 
-    Optional<T> getById(Object id) throws SQLException;
+    Optional<T> getById(Object id) throws Exception;
 
-    void save(T data);
+    void save(T data) throws Exception;
 
-    void delete(Object id);
+    void delete(int id) throws Exception;
 }
