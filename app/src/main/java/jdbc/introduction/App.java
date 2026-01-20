de107cd7c189c065e79475a849308c78f4c0a5bc
Marco Osorio <Marco.osorio86@hotmail.com>
2026-01-20 15:38:42 -0500

    Se prueba "commit" y "rollback" de transacciones

--- app/src/main/java/jdbc/introduction/App.java
+++ app/src/main/java/jdbc/introduction/App.java
@@ -18,20 +18,34 @@ public class App {
     public static void main(String[] args) throws Exception {
         try (Connection connection = DatabaseConnection.getInstance()) {
             System.out.println("Conexión a la base de datos realizada");
+            if (connection.getAutoCommit()) {
+                connection.setAutoCommit(false);
+            }
+            try {
+                Repository<Employee> repo = new EmployeeRepository(connection);
+                System.out.println("Insertando empleados");
+                repo.save(Employee.builder()
+                        .firstName("Anastasia")
+                        .paSurname("Banamerina")
+                        .email("bananaana@aaa.a")
+                        .salary(8000.00f)
+                        .curp("ABN123ASDPLZ1W2K03")
+                        .build());
+                printResultsList(repo.getAll(), Employee.class);
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+                throw new RuntimeException(e);
+            }
             //JDBCUtils.simpleInsert(connection, "employees", new String[]{"first_name", "pa_surname", "ma_surname", "email", "salary"}, new Object[]{"Marco", "Osorio", "Naranjo", "marco@example.com", 55000.00});
             //JDBCUtils.simpleUpdate(connection, "employees", new String[]{"first_name", "pa_surname", "ma_surname"}, new Object[]{"Manuela", "Lopera", "Jaramillo"}, List.of("id = 11"));
             //JDBCUtils.simpleDelete(connection, "employees", List.of("id = 10"));
             //JDBCUtils.simpleSelect(connection, "employees", List.of("id", "first_name", "pa_surname", "ma_surname", "email", "salary"), null);
             //JDBCUtils.printResultSet(resultSet);
-            Repository<Employee> repo = new EmployeeRepository();
-            //repo.save(Employee.builder().id(13).firstName("Ana").paSurname("Banana").email("bananaana@aaa.a").salary(8000.00f).build());
             //repo.delete(15);
-            System.out.println(repo.getById(2).orElse(new Employee()));
-            printResultsList(repo.getAll(), Employee.class);
-
-            SwingApp app = new SwingApp();
-            app.setVisible(true);
-
+            //System.out.println(repo.getById(2).orElse(new Employee()));
+            //SwingApp app = new SwingApp();
+            //app.setVisible(true);
         }
     }
 }
